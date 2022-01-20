c65e85922ca55d86e3bcfdd1ba17c438fea66aff
emily-cheung1 <emilycheung777@gmail.com>
2022-01-19 21:15:44 -0800

    testing

--- portfolio.js
+++ portfolio.js
@@ -5,3 +5,4 @@
 //   built in function that scrolls the page to bring the element it is being called upon into view i.e. body
 //     document.body.scrollIntoView({behavior: "smooth"});
 // };
+function test

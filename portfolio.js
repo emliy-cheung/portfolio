4d62b9ba8ac450a64044f28fc372344b38d1e684
emily-cheung1 <emilycheung777@gmail.com>
2022-01-19 20:59:33 -0800

    no top button

--- portfolio.js
+++ portfolio.js
@@ -1,7 +1,7 @@
-function toTheTop() {
-    // document.body.scrollTop = 0;
-    // document.documentElement.scrollTop = 0;
+// function toTheTop() {
+//     document.body.scrollTop = 0;
+//     document.documentElement.scrollTop = 0;
   
-  //built in function that scrolls the page to bring the element it is being called upon into view i.e. body
-    document.body.scrollIntoView({behavior: "smooth"});
-};
+//   built in function that scrolls the page to bring the element it is being called upon into view i.e. body
+//     document.body.scrollIntoView({behavior: "smooth"});
+// };
